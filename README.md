8bc8526b7699ece5692716c60ed59cbfe49c8771
Dmitri Avdejev <dmitriav@hotmail.com>
2022-03-01 11:17:18 +0000

    Initial project commit with basic structure.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vert.x Application
+
+A simple application to demonstrate use of Vert.x components.
+
+To build and test the application you need to have Java 11 and Maven 3 installed.
+
+Use the executable JAR to start:
+```
+java -jar vertx-application-1.0.0-executable.jar
+```
---
+++ src/main/java/com/dmitriav/vertx/Application.java
@@ -0,0 +1,14 @@
+package com.dmitriav.vertx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+
+  public static void main(String... arguments) {
+    logger.info("Application started");
+  }
+}
